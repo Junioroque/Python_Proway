0230d1288f40138d37f6c73982b6bd4d29b319ba
Junio Roque <jcjunioroque@gmail.com>
2026-05-19 10:05:57 -0300

    fundamentos

---
+++ Fundamentos_Python/Operadores_Matematicos.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um numero inteiro: "))
+n2 = int(input("Digite outro numero inteiro: "))
+
+print("Resultado da Adição: " + str(n1 + n2))
+print("Resultado da Subtração: " + str(n1 - n2))
+print("Resultado da Multiplicação: " + str(n1 * n2))
+print("Resultado da Divisão: " + str(n1 / n2))
