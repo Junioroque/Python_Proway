ae886a5405749a9237aadae9501e0f65ab71d48f
Junio Roque <jcjunioroque@gmail.com>
2026-05-21 21:51:24 -0300

    Variaveis

--- Fundamentos_Python/Variaveis.py
+++ Fundamentos_Python/Variaveis.py
@@ -1,3 +1,23 @@
-#Tipo Inteiro, Float, String, Double
+#Tipo Inteiro, Float, String, Double, Bool
+# String
 nome = input("Infome o seu nome: ");
 print(nome); 
+print(type(nome));
+
+print("============================");
+# Inteiro
+numero = 5;
+print(numero);
+print(type(numero));
+
+print("============================");
+# Float
+numeroFloat = 5.0;
+print(numeroFloat);
+print(type(numeroFloat));
+
+print("============================");
+# bool
+a = True;
+print(a);
+print(type(a));
