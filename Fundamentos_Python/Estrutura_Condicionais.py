7d5b6267ad7f78bd669e9baf63713efa9833359c
Junio Roque <jcjunioroque@gmail.com>
2026-05-26 22:46:42 -0300

    while

--- Fundamentos_Python/Estrutura_Condicionais.py
+++ Fundamentos_Python/Estrutura_Condicionais.py
@@ -4,4 +4,16 @@ numero_2 = int(input("Informe outro valor inteiro: "));
 if( numero_1 == numero_2):
     print("Os valores informados são iguais.");
 else:
-    print("Os valores são diferentes.");
+     if(numero_1 > numero_2):
+        print("O primeiro numero e maior que o segundo.");
+     else:
+        print("O segundo numero e maior que o primeiro.");
+
+#Resumir a condição
+
+if(numero_1 == numero_2):
+    print("Numeros iguais.");
+elif(numero_1 > numero_2):
+    print("Primeiro numero e maior.");
+else:
+    print("O segundo numero e maior.");
---
+++ Fundamentos_Python/Laco_repeticao.py
@@ -0,0 +1,12 @@
+# Laço de repetição
+# While
+numero = 1;
+while (numero <= 10):
+    if(numero % 2 == 0):
+        print("Par: " + str(numero));
+    else:
+        print("Impar: " + str(numero));
+    
+    numero+= 1;     
+    
+# 
