c082cbee3523afd98a4c805e6bb3f2a28fed9c19
Junio Roque <jcjunioroque@gmail.com>
2026-05-26 22:02:37 -0300

    condicionais

---
+++ Fundamentos_Python/Estrutura_Condicionais.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Informe um valor inteiro: "));
+numero_2 = int(input("Informe outro valor inteiro: "));
+
+if( numero_1 == numero_2):
+    print("Os valores informados são iguais.");
+else:
+    print("Os valores são diferentes.");
--- Fundamentos_Python/Operadores_Matematico_Melhorados.py
+++ Fundamentos_Python/Operadores_Matematico_Melhorados.py
@@ -2,3 +2,4 @@ n1 = int(input("Informe um valor inteiro: "));
 n2 = int(input("Informe outro valor inteiro: "));
 
 print("Resultado da divisão : %.2f" % (n1 / n2));
+print(f"Resultado da divisão : {(n1 / n2):.3f}");
