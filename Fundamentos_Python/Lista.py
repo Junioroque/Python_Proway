6f3bfe414b244a0fcaa07c0ba580994da5ed37ad
Junio Roque <jcjunioroque@gmail.com>
2026-05-27 23:20:21 -0300

    Lista Percorre

--- Fundamentos_Python/Lista.py
+++ Fundamentos_Python/Lista.py
@@ -7,3 +7,24 @@ print(itens);
 print('------------------------');
 
 print(itens[1]);
+
+print('------------------------');
+
+# Manipular lista
+# Insert pode escolher a posição
+itens.insert(0, 'CPU');
+itens.insert(4, 'Placa Mãe');
+print(itens);
+
+# Append sempre insere na ultima lista
+itens.append('Placa Video');
+print(itens)
+
+
+# Pop remove item da lista pode escolher a posição;
+itens.pop(2);
+print(itens);
+
+# Remove o itens da lista pelo nome
+itens.remove('Placa Video');
+print(itens);
---
+++ Fundamentos_Python/Lista_Percorrer.py
@@ -0,0 +1,3 @@
+# Percorrer a lista, quantos itens tem
+itens = ['Processador', 'Mouse', ' Teclado', 'Monitor', 'Placa Mãe'];
+print(len(itens));
