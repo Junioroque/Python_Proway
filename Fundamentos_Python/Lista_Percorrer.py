d12b74004d54c67d7da7e56b2a53e077ba279d64
Junio Roque <jcjunioroque@gmail.com>
2026-05-27 23:37:27 -0300

    atualizando

--- Fundamentos_Python/Lista_Percorrer.py
+++ Fundamentos_Python/Lista_Percorrer.py
@@ -1,3 +1,22 @@
 # Percorrer a lista, quantos itens tem
-itens = ['Processador', 'Mouse', ' Teclado', 'Monitor', 'Placa Mãe'];
+itens = ['Processador', 'Mouse', 'Teclado', 'Monitor', 'Placa Mãe'];
 print(len(itens));
+
+for x in range(0, len(itens)):
+    print(itens[x]);
+    
+print('-------------------------');
+
+existe = False;
+produto = input('Qual item deseja excluir da lista? ');
+for x in range(0,len(itens)):
+    if (produto.upper() == itens[x].upper()):
+        itens.pop(x);
+        existe = True;
+        break
+    
+print(itens);
+if (existe):
+    print(produto+ ' foi excluido da lista.');
+else:
+    print(produto + ' não existe na lista.');
