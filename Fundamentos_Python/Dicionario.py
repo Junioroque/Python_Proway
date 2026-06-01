f99ea407ecfb979e5f94db079a4af529c9f4418c
Junio Roque <jcjunioroque@gmail.com>
2026-06-01 00:25:36 -0300

    atualizando dicionario

--- Fundamentos_Python/Dicionario.py
+++ Fundamentos_Python/Dicionario.py
@@ -1,6 +1,31 @@
 dicionario = {1, 2, 3};
 
-print(type(dicionario));
+#print(type(dicionario));
 
-numero = {'um' : 'one', 2 : 'two', 'tres' : 'three'};
-print(numero[2]);
+numero = {1 : 'one', 2 : 'two', 3 : 'three'};
+#print(numero[3]);
+valor = int(input("Digite uma posição que deseja: "));
+
+if(numero.get(valor) == None):
+    print("Posição inexistente!");
+else:
+    print(numero.get(valor));
+ 
+print('---------------------------');
+   
+# Adicionar uma nova chave
+
+print(numero);
+print('');
+numero[4] = 'four';
+print(numero);
+
+indice = int(input('Qual posição deseja excluir? '));
+print(numero.pop(indice, 'Posição Inexistente!'));
+
+print(numero);
+
+numero_2 = {7 : 'seven', 8 : 'height'};
+
+numero.update(numero_2);
+print(numero)
