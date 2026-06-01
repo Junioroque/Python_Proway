0f26609c00bcfa3ddd811e90f088313a778cb009
Junio Roque <jcjunioroque@gmail.com>
2026-05-31 21:38:27 -0300

    atualizando exercicio

--- Fundamentos_Python/Tuplas.py
+++ Fundamentos_Python/Tuplas.py
@@ -12,31 +12,22 @@ for x in range(0, len(tupla)):
     print(tupla[x]);
     
     
-dia_semana = ('domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado');
-print(dia_semana[2]);
+dia_semana = ('dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab');
+#print(dia_semana[2]);
 
 print('-------------------------');
-print('Dias da Semana');
-
-for x in range(0, len(dia_semana)):
-    if(dia_semana[x] == "domingo"):
-        print(str(x+1) +' Domingo');
-    elif(dia_semana[x] == "segunda"):
-        print(str(x+1) +' Segunda-feira');
-    elif(dia_semana[x] == "terça"):
-        print(str(x+1) +' Terça-feira');
-    elif(dia_semana[x] == "quarta"):
-        print(str(x+1) +' Quarta-feira');
-    elif(dia_semana[x] == "quinta"):
-        print(str(x+1) +' Quinta-feira');
-    elif(dia_semana[x] == "sexta"):
-        print(str(x+1) +' Sexta-feira');
-    else:
-        print(str(x+1) +'Sabado');
-        
 # Faça um exercicio que o usuário informe o dia da 
 # semana e retorne a posição em que se encontra do item.
 
-posicaoItem = input("Informe o dia da Semana: ");
+print('[Dom | Seg | Ter | Qua | Qui | Sex | Sab]');
+opcao = input("Informe o dia da semana desejado: ");
+
+
+for x in range(0, len(dia_semana)):
+    if(dia_semana[x].upper() == opcao.upper()):
+        print(str(x+1));
+        
+
+
     
 
